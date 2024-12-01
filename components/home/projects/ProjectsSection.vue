4230e06633c94692fb65f4644fce6b25da9c2ff7
khaldoun36 <khaldoun36@gmail.com>
2024-12-01 10:14:34 +0400

    home page almost done

--- components/home/projects/ProjectsSection.vue
+++ components/home/projects/ProjectsSection.vue
@@ -3,6 +3,41 @@
         <h2 class="max-w-[40ch] text-balance text-3xl md:text-4xl lg:text-5xl">
             {{ projectsSection?.title }}
         </h2>
+        <div class="mt-5 md:mt-6 lg:mt-8">
+            <ContentList :path="`/${currentLocale}/projects`" v-slot="{ list }">
+                <div v-for="article in list.slice(0, 3)" :key="article.path">
+                    <NuxtLink
+                        to="https://www.theverge.com/"
+                        class="flex flex-col border-b border-white/10 py-5 md:py-6 lg:flex-row lg:items-start lg:gap-20 lg:py-8"
+                    >
+                        <NuxtImg
+                            :src="article.thumbnail"
+                            :alt="article.title"
+                            width="4000"
+                            height="3000"
+                            class="aspect-[4/3] flex-1 overflow-clip rounded border border-white/10 object-cover lg:max-w-[50%]"
+                        />
+
+                        <div class="flex flex-col">
+                            <div>
+                                <h3 class="mt-5 text-balance text-2xl lg:mt-0">
+                                    {{ article.title }}
+                                </h3>
+                                <p class="mt-3 text-base md:text-lg">
+                                    {{ article.seoDescription }}
+                                </p>
+                            </div>
+                            <button
+                                class="btn mt-8 !hidden self-end lg:!flex"
+                                data-variant="primary"
+                            >
+                                Read More
+                            </button>
+                        </div>
+                    </NuxtLink>
+                </div>
+            </ContentList>
+        </div>
     </section>
 </template>
 

4d1d552cbd460acfcf31218d1e805207da68ecb4
khaldoun36 <khaldoun36@gmail.com>
2024-12-06 09:14:30 +0400

    fix 6

--- pages/services/[slug].vue
+++ pages/services/[slug].vue
@@ -1,30 +1,45 @@
 <template>
     <main
         class="relative mt-40 grid gap-10 md:mt-44 md:gap-12 lg:mt-52 lg:grid-cols-[1.5fr_1fr] lg:gap-16"
-        v-if="article"
     >
         <div class="self-start lg:sticky lg:top-20 lg:col-start-2">
             <h1
                 class="max-w-[40ch] text-balance text-4xl md:text-7xl lg:text-7.5xl"
             >
-                {{ article?.title }}
+                {{ article.title }}
             </h1>
-            <p class="mt-8 max-w-[45ch] text-balance text-base md:text-lg">
-                {{ article?.description }}
+            <p
+                v-for="item in article.description"
+                :key="item"
+                class="mt-8 max-w-[45ch] text-balance text-base md:text-lg"
+            >
+                {{ item }}
             </p>
         </div>
+        <div class="space-y-8 lg:row-start-1">
+            <NuxtImg
+                v-for="image in article.images"
+                :src="image"
+                :key="image"
+                width="3000"
+                height="4000"
+                class="aspect-[3/4] overflow-hidden rounded border border-white/10"
+            />
+        </div>
     </main>
-    <div v-else>Loading... {{ error }}</div>
 </template>
 
 <script setup>
 const { locale } = useI18n();
 const currentLocale = computed(() => locale.value);
 
-const { data: article, error } = await useAsyncData(
-    "services-article",
+const route = useRoute();
+
+// Watch for locale changes and refetch article data
+const { data: article } = await useAsyncData(
+    "project-article",
     () =>
-        queryContent(`/${currentLocale.value}/services`)
+        queryContent(`/${currentLocale.value}/projects`)
             .where({ path: route.params.slug })
             .findOne(),
     {
@@ -32,15 +47,11 @@ const { data: article, error } = await useAsyncData(
     }
 );
 
-if (error.value) {
-    console.error("Failed to fetch article:", error.value);
-    // Handle error, maybe redirect or show an error page
-}
-// useSeoMeta({
-//     title: article.value?.title || "Default Title",
-//     description: article.value?.description || "Default Description",
-//     ogImage: article.value?.images?.[0] || "default-image.jpg",
-// });
+useSeoMeta({
+    title: `${article.title}`,
+    description: `${article.description}`,
+    ogImage: `${article.thumbnail}`,
+});
 </script>
 
 <style scoped></style>

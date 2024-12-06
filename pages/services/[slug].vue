7b493e7bc71c407c09d1ad5a71904cb8b2e85c93
khaldoun36 <khaldoun36@gmail.com>
2024-12-06 09:18:55 +0400

    fixed please

--- pages/services/[slug].vue
+++ pages/services/[slug].vue
@@ -1,45 +1,41 @@
 <template>
     <main
         class="relative mt-40 grid gap-10 md:mt-44 md:gap-12 lg:mt-52 lg:grid-cols-[1.5fr_1fr] lg:gap-16"
+        v-if="article"
     >
         <div class="self-start lg:sticky lg:top-20 lg:col-start-2">
             <h1
                 class="max-w-[40ch] text-balance text-4xl md:text-7xl lg:text-7.5xl"
             >
-                {{ article.title }}
+                {{ article?.title }}
             </h1>
-            <p
-                v-for="item in article.description"
-                :key="item"
-                class="mt-8 max-w-[45ch] text-balance text-base md:text-lg"
-            >
-                {{ item }}
+            <p class="mt-8 max-w-[45ch] text-balance text-base md:text-lg">
+                {{ article?.description }}
             </p>
         </div>
         <div class="space-y-8 lg:row-start-1">
             <NuxtImg
-                v-for="image in article.images"
+                v-for="(image, index) in article.images"
                 :src="image"
-                :key="image"
+                :key="index"
                 width="3000"
                 height="4000"
                 class="aspect-[3/4] overflow-hidden rounded border border-white/10"
             />
         </div>
     </main>
+    <div v-else>Loading... {{ error }}</div>
 </template>
 
 <script setup>
 const { locale } = useI18n();
 const currentLocale = computed(() => locale.value);
-
 const route = useRoute();
 
-// Watch for locale changes and refetch article data
-const { data: article } = await useAsyncData(
-    "project-article",
+const { data: article, error } = await useAsyncData(
+    "services-article",
     () =>
-        queryContent(`/${currentLocale.value}/projects`)
+        queryContent(`/${currentLocale.value}/services`)
             .where({ path: route.params.slug })
             .findOne(),
     {
@@ -47,10 +43,14 @@ const { data: article } = await useAsyncData(
     }
 );
 
+if (error.value) {
+    console.error("Failed to fetch article:", error.value);
+    // Handle error, maybe redirect or show an error page
+}
 useSeoMeta({
-    title: `${article.title}`,
-    description: `${article.description}`,
-    ogImage: `${article.thumbnail}`,
+    title: article.value?.title || "Default Title",
+    description: article.value?.description || "Default Description",
+    ogImage: article.value?.images?.[0] || "default-image.jpg",
 });
 </script>
 
